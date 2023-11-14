53a0d60992111044d806792e5e6c5ceecf8c46b9
LupoEZ <115219094+LupoEZ@users.noreply.github.com>
2023-11-14 14:41:34 +0100

    new backend dir

---
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -0,0 +1,20 @@
+package com.example.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static class Calculator {
+
+		public int sum(int a, int b) {
+			return a+b;
+		}
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
