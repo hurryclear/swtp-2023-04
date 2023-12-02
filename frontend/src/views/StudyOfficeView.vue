e0682bb49b3ab0e9e1001441f09324863b9d7f69
Lukas Streubel <hf45arax@studserv.uni-leipzig.de>
2023-12-02 18:46:57 +0100

    implemented FormDisplay.vue in StudyOfficeView.vue

--- frontend/src/views/StudyOfficeView.vue
+++ frontend/src/views/StudyOfficeView.vue
@@ -1,15 +1,17 @@
-<script setup>
-
-</script>
-
 <template>
-    <v-app>
-      <v-btn>
-        Test
-      </v-btn>
+    <v-app class="wrapper">
+      <h1>Offene Anträge</h1>
+      <FormDisplay/>
     </v-app>
 </template>
 
-<style scoped>
+<script setup>
+  import FormDisplay from "@/components/FormDisplay.vue";
 
+</script>
+
+<style scoped>
+  .wrapper {
+    margin-left: 5%;
+  }
 </style>
