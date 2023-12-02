568d62788234f9092ec7af0bf60a8fee698ce4e5
Lukas Streubel <hf45arax@studserv.uni-leipzig.de>
2023-12-02 18:45:34 +0100

    implemented FormDisplay.vue component with simple example list

---
+++ frontend/src/components/FormDisplay.vue
@@ -0,0 +1,16 @@
+<template>
+  <li v-for="tests in testArray" v-bind:key="tests" class="list">
+    {{ tests }}
+  </li>
+</template>
+
+<script setup>
+  const testArray = [1, 2, 3, 4, 5]
+</script>
+
+<style scoped>
+  .list {
+    padding: 30px;
+    margin-left: 5%;
+  }
+</style>
