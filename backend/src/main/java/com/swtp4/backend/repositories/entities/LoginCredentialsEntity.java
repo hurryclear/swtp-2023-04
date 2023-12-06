e44a057c2a23936b51541a3ff916e80f6b88d7d2
LupoEZ <115219094+LupoEZ@users.noreply.github.com>
2023-12-06 19:18:45 +0100

    new backend file, added entity classes

--- backend/src/main/java/com/swtp4/backend/repositories/entities/LoginCredentialsEntity.java
+++ backend/src/main/java/com/swtp4/backend/repositories/entities/LoginCredentialsEntity.java
@@ -1,6 +1,24 @@
 package com.swtp4.backend.repositories.entities;
 
-public class LoginDatenEntity {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-    
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "login_credentials")
+public class LoginCredentialsEntity {
+
+    @Id
+    private String username;
+
+    private String password;
+
 }
--- backend/src/main/java/com/swtp4/backend/repositories/entities/UniversityModulesEntity.java
+++ backend/src/main/java/com/swtp4/backend/repositories/entities/UniversityModulesEntity.java
@@ -1,2 +1,34 @@
-package com.swtp4.backend.repositories.entities;public class UniversityModulesEntity {
+package com.swtp4.backend.repositories.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.sql.Blob;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "university_modules")
+public class UniversityModulesEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String number;
+
+    @JoinColumn(name = "applications_id")
+    private UUID application_id;
+
+    private String title;
+
+    @Lob
+    private Blob description_pdf;
+
+    private int credits;
 }
