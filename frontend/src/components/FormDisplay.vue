2355705da4a1b3eb6c92e58a8c0a65a315281d8d
Lukas Streubel <hf45arax@studserv.uni-leipzig.de>
2023-12-06 17:17:35 +0100

    added a second test form and dynamically displayed both forms with improved style

--- frontend/src/components/FormDisplay.vue
+++ frontend/src/components/FormDisplay.vue
@@ -1,22 +1,30 @@
 <template>
-  <li class="list">
-    <v-btn class="edit-button">
-      Bearbeiten
-    </v-btn>
-    Antrag: {{ json.universityData.universityName }}
-  </li>
+  <ul v-for="form in forms" v-bind:key="form" class="list">
+    <v-row>
+      Studiengang: {{ form.universityData.studyProgram }}
+      <br>
+      Anzahl der Module: {{ form.moduleFormsData.length }}
+      <v-btn class="edit-button">
+        Bearbeiten
+      </v-btn>
+    </v-row>
+  </ul>
 </template>
 
 <script setup>
-  import json from '@/assets/test_Form.json'
+  import test1 from '@/assets/test_Form.json'
+  import test2 from '@/assets/test_Form2.json'
+
+  const forms = {test1, test2}
 </script>
 
 <style scoped>
   .list {
-    padding: 30px;
+    padding: 2rem;
   }
 
   .edit-button {
-    margin-right: 0.5rem;
+    margin-left: 5%;
+    align-self: center;
   }
 </style>
