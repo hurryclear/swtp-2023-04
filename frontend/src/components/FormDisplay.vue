6712bf328c91a8e1f4cdc71269734000e00b2f8a
Lukas Streubel <hf45arax@studserv.uni-leipzig.de>
2023-12-08 15:45:41 +0100

    changed display from table to list, added logging function to edit button

--- frontend/src/components/FormDisplay.vue
+++ frontend/src/components/FormDisplay.vue
@@ -1,14 +1,29 @@
 <template>
-  <ul v-for="form in forms" v-bind:key="form" class="list">
-    <v-row>
-      Studiengang: {{ form.universityData.studyProgram }}
-      <br>
-      Anzahl der Module: {{ form.moduleFormsData.length }}
-      <v-btn class="edit-button">
-        Bearbeiten
-      </v-btn>
-    </v-row>
-  </ul>
+  <v-table class="table">
+    <thead>
+    <tr>
+      <th style="text-align: left">Universität</th>
+      <th style="text-align: center">Anzahl der Module</th>
+      <th style="text-align: right;">Bisheriger Studiengang</th>
+    </tr>
+    </thead>
+    <tr v-for="form in forms" v-bind:key="form" class="table-row">
+      <td style="text-align: left">
+        {{ form.universityData.universityName }}
+      </td>
+      <td style="text-align: center">
+        {{ form.moduleFormsData.length }}
+      </td>
+      <td style="text-align: right">
+        {{ form.universityData.studyProgram }}
+      </td>
+      <td>
+        <v-btn @click="openEditMenu(form)">
+          Bearbeiten
+        </v-btn>
+      </td>
+    </tr>
+  </v-table>
 </template>
 
 <script setup>
@@ -16,15 +31,32 @@
   import test2 from '@/assets/test_Form2.json'
 
   const forms = {test1, test2}
+
+  function openEditMenu(form) {
+    //edit menu not implemented yet
+    console.log(form)
+  }
 </script>
 
 <style scoped>
-  .list {
-    padding: 2rem;
+  .table {
+    table-layout: fixed;
+    margin-right: 5rem;
+    width: 40%;
+    border-collapse: collapse;
   }
 
-  .edit-button {
-    margin-left: 5%;
-    align-self: center;
+  td {
+    padding: 1rem;
+    width: 33%;
+  }
+
+  tr {
+    border: solid;
+    border-width: 1px 0;
+  }
+
+  .table-row {
+    padding-top: 20%;
   }
 </style>
