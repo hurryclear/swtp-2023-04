2f1ddc530f8882bafefb2cd9748b2fd35f18f15a
Lukas Streubel <hf45arax@studserv.uni-leipzig.de>
2023-12-12 19:17:15 +0100

    implemented open-edit-menu event

--- frontend/src/components/FormDisplay.vue
+++ frontend/src/components/FormDisplay.vue
@@ -2,19 +2,19 @@
   <v-table class="table">
     <thead>
     <tr>
-      <th style="text-align: left">Universität</th>
-      <th style="text-align: center">Anzahl der Module</th>
+      <th style="text-align: left;">Universität</th>
+      <th style="text-align: center;">Anzahl der Module</th>
       <th style="text-align: right;">Bisheriger Studiengang</th>
     </tr>
     </thead>
     <tr v-for="form in forms" v-bind:key="form" class="table-row">
-      <td style="text-align: left">
+      <td style="text-align: left;">
         {{ form.universityData.universityName }}
       </td>
-      <td style="text-align: center">
+      <td style="text-align: center;">
         {{ form.moduleFormsData.length }}
       </td>
-      <td style="text-align: right">
+      <td style="text-align: right;">
         {{ form.universityData.studyProgram }}
       </td>
       <td>
@@ -26,15 +26,21 @@
   </v-table>
 </template>
 
-<script setup>
+<script>
   import test1 from '@/assets/test_Form.json'
   import test2 from '@/assets/test_Form2.json'
-
-  const forms = {test1, test2}
-
-  function openEditMenu(form) {
-    //edit menu not implemented yet
-    console.log(form)
+  export default {
+    data() {
+      return {
+        forms: [test1, test2]
+      }
+    },
+    methods: {
+      openEditMenu(form) {
+        //const cleanJSON = JSON.stringify(form)
+        this.$emit('open-edit-menu', form);
+      }
+    }
   }
 </script>
 
@@ -43,17 +49,11 @@
     table-layout: fixed;
     margin-right: 5rem;
     width: 40%;
-    border-collapse: collapse;
   }
 
   td {
     padding: 1rem;
     width: 33%;
-  }
-
-  tr {
-    border: solid;
-    border-width: 1px 0;
   }
 
   .table-row {
