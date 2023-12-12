29cf7c765f3cdb947294984bc80ca78e57e47820
Lukas Streubel <hf45arax@studserv.uni-leipzig.de>
2023-12-12 19:16:41 +0100

    implemented event listener for FormDisplay.vue and triggered function

--- frontend/src/views/StudyOfficeView.vue
+++ frontend/src/views/StudyOfficeView.vue
@@ -1,12 +1,32 @@
 <template>
-    <v-container fluid="true" class="wrapper">
+    <v-container fluid class="wrapper">
       <h1>Offene Anträge</h1>
-      <FormDisplay/>
+      <FormDisplay @open-edit-menu="displayEditMenu"/>
+      <EditMenu v-if="isDisplayed" :form="formContent"/>
     </v-container>
 </template>
 
-<script setup>
-  import FormDisplay from "@/components/FormDisplay.vue";
+<script>
+  import EditMenu from "@/components/EditMenu.vue";
+  import FormDisplay from "@/components/FormDisplay.vue"
+  export default {
+    components: {EditMenu, FormDisplay},
+    data() {
+      return {
+        isDisplayed: false,
+        formContent: {}
+      }
+    },
+    methods: {
+      displayEditMenu(form) {
+        this.formContent = form;
+        this.isDisplayed = true;
+      }
+    }
+  }
+
+
+
 </script>
 
 <style scoped>
