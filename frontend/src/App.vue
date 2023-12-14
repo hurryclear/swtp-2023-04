931bb35366716d2063fdd19c7471a153045cbd3c
Simon Knoll <116833239+simon-JK@users.noreply.github.com>
2023-12-14 11:19:55 +0100

    test connection between frontend and backend container using axios request and GreetingController

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,25 +1,80 @@
+<!--<template>-->
+<!--    <v-app class="v-application">-->
+<!--      <DarkThemeToggle/>-->
+<!--      <br/>-->
+<!--      <ApplicationForm/>-->
+<!--    </v-app>-->
+<!--</template>-->
+
+<!--<script>-->
+<!--import DarkThemeToggle from "@/components/DarkThemeToggle.vue";-->
+<!--import ApplicationForm from "@/components/ApplicationForm.vue";-->
+<!--import {defineComponent} from "vue";-->
+
+<!--export default defineComponent({-->
+<!--  components: {DarkThemeToggle, ApplicationForm, }-->
+<!--})-->
+
+<!--</script>-->
+
+<!--<style>-->
+<!--  .v-application {-->
+<!--    transition: background-color 1s ease, color 1s ease; /*Dark Mode Transition*/-->
+<!--    padding: 0.5rem;-->
+<!--  }-->
+<!--</style>-->
+
 <template>
-    <v-app class="v-application">
-      <DarkThemeToggle/>
-      <br/>
-      <ApplicationForm/>
-    </v-app>
+  <v-app class="v-application">
+    <DarkThemeToggle/>
+    <br/>
+    <!-- Display the greeting message from the backend -->
+    <div v-if="greeting" class="greeting-message">
+      {{ greeting }}
+    </div>
+    <br/>
+    <ApplicationForm/>
+  </v-app>
 </template>
 
 <script>
 import DarkThemeToggle from "@/components/DarkThemeToggle.vue";
 import ApplicationForm from "@/components/ApplicationForm.vue";
-import {defineComponent} from "vue";
+import { defineComponent, ref, onMounted } from "vue";
+import axios from 'axios';
 
 export default defineComponent({
-  components: {DarkThemeToggle, ApplicationForm, }
-})
+  components: { DarkThemeToggle, ApplicationForm },
+  setup() {
+    const greeting = ref(''); // Reactive reference for the greeting message
 
+    // Fetch the greeting message from the backend when the component is mounted
+    onMounted(() => {
+      axios.get('http://backend:8080/greeting')
+          .then(response => {
+            greeting.value = response.data;
+          })
+          .catch(error => {
+            console.error(error);
+          });
+    });
+
+    return {
+      greeting
+    };
+  }
+})
 </script>
 
 <style>
-  .v-application {
-    transition: background-color 1s ease, color 1s ease; /*Dark Mode Transition*/
-    padding: 0.5rem;
-  }
+.v-application {
+  transition: background-color 1s ease, color 1s ease; /*Dark Mode Transition*/
+  padding: 0.5rem;
+}
+.greeting-message {
+  margin-bottom: 1rem;
+  padding: 0.5rem;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+}
 </style>
