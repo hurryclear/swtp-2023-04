7c0e949706af125e3d8add017bdd522419cba4e8
Simon Knoll <116833239+simon-JK@users.noreply.github.com>
2024-01-19 19:24:14 +0100

    added Studienbüro and Prüfungsausschuss Credentials

---
+++ backend/src/main/java/com/swtp4/backend/security/UserInitializer.java
@@ -0,0 +1,46 @@
+package com.swtp4.backend.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserInitializer implements CommandLineRunner {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Value("${office.username}")
+    private String officeUsername;
+    @Value("${office.password}")
+    private String officePassword;
+    @Value("${committee.username}")
+    private String committeeUsername;
+    @Value("${committee.password}")
+    private String committeePassword;
+
+    @Autowired
+    public UserInitializer(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        // Create a user and save in the database
+        UserEntity office = new UserEntity();
+        office.setUsername(officeUsername);
+        office.setPassword(passwordEncoder.encode(officePassword));
+        office.setRole(Role.OFFICE);
+        userRepository.save(office);
+
+        UserEntity committee = new UserEntity();
+        committee.setUsername(committeeUsername);
+        committee.setPassword(passwordEncoder.encode(committeePassword));
+        committee.setRole(Role.COMMITTEE);
+        userRepository.save(committee);
+    }
+}
+
