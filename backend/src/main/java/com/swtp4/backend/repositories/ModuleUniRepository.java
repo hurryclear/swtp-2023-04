16175ddd6aaccf588f3f8da880e11c885683b3f7
Simon Knoll <116833239+simon-JK@users.noreply.github.com>
2024-02-28 12:12:30 +0100

    Majors and Modules can now be fetched UniData IntegrationTests pass

--- backend/src/main/java/com/swtp4/backend/repositories/ModuleUniRepository.java
+++ backend/src/main/java/com/swtp4/backend/repositories/ModuleUniRepository.java
@@ -1,12 +1,17 @@
 package com.swtp4.backend.repositories;
 
+import com.swtp4.backend.repositories.entities.MajorUniEntity;
 import com.swtp4.backend.repositories.entities.ModuleUniEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ModuleUniRepository extends JpaRepository<ModuleUniEntity, String> {
 
     ModuleUniEntity findByNumber(String number);
     ModuleUniEntity findByName(String name);
+
+    List<ModuleUniEntity> findByMajorUniEntity(MajorUniEntity majorUniEntity);
 }
