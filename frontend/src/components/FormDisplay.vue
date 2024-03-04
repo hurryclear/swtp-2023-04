49a958590c4c1e87eeef7085c12d58b4bce0adec
Robert <rs75lovo@studserv.uni-leipzig.de>
2024-03-04 11:39:35 +0100

    + removed unmatched curly Brace in FormDisplay.vue

--- frontend/src/components/FormDisplay.vue
+++ frontend/src/components/FormDisplay.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>{{$t("studentAffairsOfficeView.openApplications")}}</h1>
+  <h1>{{ $t("studentAffairsOfficeView.openApplications") }}</h1>
   <v-data-table :headers="headers" :items="forms" item-key="id" class="table">
     <template v-slot:[`item.actions`]="{ item }">
       <v-btn @click="openEditMenu(item)" icon="mdi-pencil"></v-btn>
@@ -15,29 +15,28 @@ import form2 from "@/assets/form-2023-12-13T6_45_30.512.json"
 import form3 from "@/assets/form-2023-12-15T10_34_51.207Z.json"
 import form4 from "@/assets/form4.json"*/
 export default {
-    data() {
-      return {
-        //formArray: [form1,form2,form3,form4],
-        //TODO i18n
-        headers: [
-          { title: "ID", key: "id" },
-          { title: "University" , key: "universityData.universityName" },
-          { title: "Number of Modules", key: "moduleFormsData.length" },
-          { title: "Previous Study Program", key: "universityData.studyProgram" },
-          { title: "Edit", value: "actions", sortable: false}
-        ]
-      }
-    },
-    computed: {
-      // Use Vuex getter to get forms with a specific status
-      forms() {
-        return this.$store.getters.formsByStatus('Offen');
-      }
-    },
-    methods: {
-      openEditMenu(form) {
-        this.$emit('open-edit-menu', form);
-      }
+  data() {
+    return {
+      //formArray: [form1,form2,form3,form4],
+      //TODO i18n
+      headers: [
+        {title: "ID", key: "id"},
+        {title: "University", key: "universityData.universityName"},
+        {title: "Number of Modules", key: "moduleFormsData.length"},
+        {title: "Previous Study Program", key: "universityData.studyProgram"},
+        {title: "Edit", value: "actions", sortable: false}
+      ]
+    }
+  },
+  computed: {
+    // Use Vuex getter to get forms with a specific status
+    forms() {
+      return this.$store.getters.formsByStatus('Offen');
+    }
+  },
+  methods: {
+    openEditMenu(form) {
+      this.$emit('open-edit-menu', form);
     }
   }
 }
@@ -45,13 +44,13 @@ export default {
 
 <style scoped>
 
-  .table {
-    width: 70rem;
-  }
+.table {
+  width: 70rem;
+}
 
-  td {
-    padding: 1rem;
-    width: 20%;
-  }
+td {
+  padding: 1rem;
+  width: 20%;
+}
 
 </style>
