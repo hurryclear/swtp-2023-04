e1dd29c63e4f49fe3e721bd28d78fb4912168bf2
Huo Jiang <ru20emez@studserv.uni-leipzig.de>
2024-03-05 15:05:17 +0100

    test

--- backend/src/main/java/com/swtp4/backend/BackendApplication.java
+++ backend/src/main/java/com/swtp4/backend/BackendApplication.java
@@ -1,5 +1,7 @@
 package com.swtp4.backend;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
+import com.swtp4.backend.services.ApplicationService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,8 +9,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendApplication {
 
 
+//	public static void main(String[] args) {
+//		SpringApplication.run(BackendApplication.class, args);
+//	}
+
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		var ctx = SpringApplication.run(BackendApplication.class, args);
+		ApplicationService applicationService = ctx.getBean(ApplicationService.class);
+		System.out.println(applicationService.getApplicationsByStatus("open"));
 	}
 
 }
