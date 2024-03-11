5d592de22dc68c6bee3213914249a0c1f06649a1
jakob <jakob.huss@gmail.com>
2024-03-11 18:25:23 +0100

    small refactoring

--- frontend/src/services/FormService.js
+++ frontend/src/services/FormService.js
@@ -2,22 +2,22 @@
 import axios from '@/plugins/axios';
 
 export default {
-    async fetchApplicationSummary(formId) {
-        try {
-            const response = await axios.get('/api/student/getApplicationSummary', {params: {formId}});
-            return response.data.form;
-        } catch (error) {
-            console.error('Error fetching Application Suummary:', error);
-            throw error;
-        }
-    },
-    async fetchPdfSummary(formId) {
-        try {
-            const response = await axios.get('/api/student/getPdfSummary', {params: {formId}, responseType: 'blob'});
-            return response.data;
-        } catch (error) {
-            console.error('Error fetching PDF Summary:', error);
-            throw error;
-        }
-    },
+  async fetchApplicationSummary(applicationId) {
+    try {
+      const response = await axios.get(`/api/student/getApplicationSummary`, {params: {applicationId}});
+      return response.data;
+    } catch (error) {
+      console.error('Error fetching application summary:', error);
+      throw error;
+    }
+  },
+  async fetchPdfSummary(applicationId) {
+    try {
+      const response = await axios.get(`/api/student/getPdfSummary`, {params: {applicationId}}, { responseType: 'blob' });
+      return response.data;
+    } catch (error) {
+      console.error('Error fetching PDF summary:', error);
+      throw error;
+    }
+  },
 };
