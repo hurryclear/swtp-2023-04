a583157a5fbef4c4c02b7d338782487c6a312780
jakob <jakob.huss@gmail.com>
2024-03-12 22:48:20 +0100

    changed param Typ to match Endpoint

--- frontend/src/services/FormService.js
+++ frontend/src/services/FormService.js
@@ -4,7 +4,7 @@ import axios from '@/plugins/axios';
 export default {
   async fetchApplicationSummary(applicationId) {
     try {
-      const response = await axios.get(`/api/student/reviewApplication`, {params: {applicationId}});
+      const response = await axios.get(`/api/student/reviewApplication`, {params: {applicationID: applicationId}});
       return response.data;
     } catch (error) {
       console.error('Error fetching application summary:', error);
