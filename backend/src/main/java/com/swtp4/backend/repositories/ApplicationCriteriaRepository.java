16df5a55adcf838fbbfa3f8fad4cdedd139523c1
Huo Jiang <ru20emez@studserv.uni-leipzig.de>
2024-03-13 15:29:43 +0100

    add more predicates and update

--- backend/src/main/java/com/swtp4/backend/repositories/ApplicationCriteriaRepository.java
+++ backend/src/main/java/com/swtp4/backend/repositories/ApplicationCriteriaRepository.java
@@ -1,6 +1,8 @@
 package com.swtp4.backend.repositories;
 
 import com.swtp4.backend.repositories.applicationDtos.OverviewApplicationDto;
+import com.swtp4.backend.repositories.dto.ApplicationDto;
+import com.swtp4.backend.repositories.entities.ApplicationEntity;
 import com.swtp4.backend.repositories.model.ApplicationPage;
 import com.swtp4.backend.repositories.model.ApplicationSearchCriteria;
 import jakarta.persistence.EntityManager;
@@ -29,16 +31,22 @@ public class ApplicationCriteriaRepository {
         this.criteriaBuilder = entityManager.getCriteriaBuilder();
     }
 
-    public Page<OverviewApplicationDto> findAllWithFilters(
+    public Page<ApplicationEntity> findAllWithFilters(
             ApplicationPage applicationPage,
             ApplicationSearchCriteria applicationSearchCriteria) {
-        CriteriaQuery<OverviewApplicationDto> criteriaQuery = criteriaBuilder.createQuery(OverviewApplicationDto.class);
-        Root<OverviewApplicationDto> overviewApplicationDtoRoot = criteriaQuery.from(OverviewApplicationDto.class);
-        Predicate predicate = getPredicate(applicationSearchCriteria, overviewApplicationDtoRoot);
-        criteriaQuery.where(predicate);
-        setOrder(applicationPage, criteriaQuery, overviewApplicationDtoRoot);
 
-        TypedQuery<OverviewApplicationDto> typedQuery = entityManager.createQuery(criteriaQuery);
+        // construct SELECT statement
+        CriteriaQuery<ApplicationEntity> criteriaQuery = criteriaBuilder.createQuery(ApplicationEntity.class);
+        Root<ApplicationEntity> applicationEntityRoot = criteriaQuery.from(ApplicationEntity.class);
+        // predicate represents the conditions that will be applied to filter the results
+        Predicate predicate = getPredicate(applicationSearchCriteria, applicationEntityRoot);
+        criteriaQuery.where(predicate);
+        //order the results
+        setOrder(applicationPage, criteriaQuery, applicationEntityRoot);
+
+        // A TypedQuery is a JPA query that is typed to return a specific result type. Here, we're creating a TypedQuery of type OverviewApplicationDto, which represents the result set of the query.
+        TypedQuery<ApplicationEntity> typedQuery = entityManager.createQuery(criteriaQuery);
+        // paging
         typedQuery.setFirstResult(applicationPage.getPageNumber() * applicationPage.getPageSize());
         typedQuery.setMaxResults(applicationPage.getPageSize());
 
@@ -49,54 +57,67 @@ public class ApplicationCriteriaRepository {
         return new PageImpl<>(typedQuery.getResultList(), pageable, applicationsCount);
     }
 
+    // filtering
     private Predicate getPredicate(ApplicationSearchCriteria applicationSearchCriteria,
-                                   Root<OverviewApplicationDto> overviewApplicationDtoRoot) {
+                                   Root<ApplicationEntity> applicationEntityRoot) {
         List<Predicate> predicates = new ArrayList<>();
+        predicates.add(criteriaBuilder.equal(applicationEntityRoot.get("applicationKeyClass").get("creator"), "Employee"));
+        predicates.add(criteriaBuilder.equal(applicationEntityRoot.get("status"),"approval finished"));
         if(Objects.nonNull(applicationSearchCriteria.getApplicationID())) {
             predicates.add(
                     criteriaBuilder.like(
-                            overviewApplicationDtoRoot.get("applicationID"),
+                            applicationEntityRoot.get("applicationKeyClass").get("id"),
                             "%" + applicationSearchCriteria.getApplicationID() + "%"));
         }
 
-        if(Objects.nonNull(applicationSearchCriteria.getStatus())) {
+//        if(Objects.nonNull(applicationSearchCriteria.getStatus())) {
+//            predicates.add(
+//                    criteriaBuilder.like(
+//                            overviewApplicationDtoRoot.get("status"),
+//                            "%" + applicationSearchCriteria.getStatus() + "%"));
+//        }
+
+        if(Objects.nonNull(applicationSearchCriteria.getUniversityName())) {
             predicates.add(
                     criteriaBuilder.like(
-                            overviewApplicationDtoRoot.get("status"),
-                            "%" + applicationSearchCriteria.getStatus() + "%"));
+                            applicationEntityRoot.get("universityName"),
+                            "%" + applicationSearchCriteria.getUniversityName() + "%"));
         }
-        if(Objects.nonNull(applicationSearchCriteria.getUniversity())) {
+        if(Objects.nonNull(applicationSearchCriteria.getUniMajor())) {
             predicates.add(
                     criteriaBuilder.like(
-                            overviewApplicationDtoRoot.get("university"),
-                            "%" + applicationSearchCriteria.getUniversity() + "%"));
+                            applicationEntityRoot.get("uniMajor"),
+                            "%" + applicationSearchCriteria.getUniMajor() + "%"));
+        }
+        //todo
+        if(Objects.nonNull(applicationSearchCriteria.getDateOfSubmission())) {
+            predicates.add(
+                    criteriaBuilder.like(
+                            applicationEntityRoot.get("dateOfSubmission"),
+                            "%" + applicationSearchCriteria.getDateOfSubmission() + "%"));
         }
         if(Objects.nonNull(applicationSearchCriteria.getDateOfSubmission())) {
             predicates.add(
                     criteriaBuilder.like(
-                            overviewApplicationDtoRoot.get("dateOfSubmission"),
+                            applicationEntityRoot.get("dateOfSubmission"),
                             "%" + applicationSearchCriteria.getDateOfSubmission() + "%"));
         }
-        if(Objects.nonNull(applicationSearchCriteria.getDateLastEdited())) {
-            predicates.add(
-                    criteriaBuilder.like(
-                            overviewApplicationDtoRoot.get("dateLastEdited"),
-                            "%" + applicationSearchCriteria.getDateLastEdited() + "%"));
-        }
-
+        criteriaBuilder.or();
         return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
     }
 
+    // sorting
     private void setOrder(ApplicationPage applicationPage,
-                          CriteriaQuery<OverviewApplicationDto> criteriaQuery,
-                          Root<OverviewApplicationDto> overviewApplicationDtoRoot) {
+                          CriteriaQuery<ApplicationEntity> criteriaQuery,
+                          Root<ApplicationEntity> applicationEntityRoot) {
         if(applicationPage.getSortDirection().equals(Sort.Direction.ASC)) {
-            criteriaQuery.orderBy(criteriaBuilder.asc(overviewApplicationDtoRoot.get(applicationPage.getSortBy())));
+            criteriaQuery.orderBy(criteriaBuilder.asc(applicationEntityRoot.get(applicationPage.getSortBy())));
         } else {
-            criteriaQuery.orderBy(criteriaBuilder.desc(overviewApplicationDtoRoot.get(applicationPage.getSortBy())));
+            criteriaQuery.orderBy(criteriaBuilder.desc(applicationEntityRoot.get(applicationPage.getSortBy())));
         }
     }
 
+    // paging
     private Pageable getPageable(ApplicationPage applicationPage) {
         Sort sort = Sort.by(applicationPage.getSortDirection(), applicationPage.getSortBy());
         return PageRequest.of(applicationPage.getPageNumber(), applicationPage.getPageSize(), sort);
