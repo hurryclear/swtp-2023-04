ecdaa5d63e833dc0ba8e1e4fc9b8390fff1c1d83
Huo Jiang <ru20emez@studserv.uni-leipzig.de>
2024-03-13 14:43:08 +0100

    update OverviewApplicationDto

--- backend/src/main/java/com/swtp4/backend/repositories/applicationDtos/OverviewApplicationDto.java
+++ backend/src/main/java/com/swtp4/backend/repositories/applicationDtos/OverviewApplicationDto.java
@@ -2,10 +2,7 @@ package com.swtp4.backend.repositories.applicationDtos;
 
 public record OverviewApplicationDto (
         String applicationID,
-        String status,
-        String dateOfSubmission,
-        String dateLastEdited,
         String university,
-        EntireApplication original,
-        EntireApplication edited
+        String dateOfSubmission,
+        String status
 ){ }
