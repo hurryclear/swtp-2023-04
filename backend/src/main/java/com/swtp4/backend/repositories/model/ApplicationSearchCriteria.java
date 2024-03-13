3fcebfeab87e96dd77c8f24b4f427d993c665c37
Huo Jiang <ru20emez@studserv.uni-leipzig.de>
2024-03-13 16:42:02 +0100

    add more attributes in search criteria

--- backend/src/main/java/com/swtp4/backend/repositories/model/ApplicationSearchCriteria.java
+++ backend/src/main/java/com/swtp4/backend/repositories/model/ApplicationSearchCriteria.java
@@ -2,12 +2,14 @@ package com.swtp4.backend.repositories.model;
 
 import lombok.Data;
 
+// contains criteria for filtering criteria
 @Data
 public class ApplicationSearchCriteria {
 
     private String applicationID;
-    private String status;
+//    private String status;
     private String dateOfSubmission;
-    private String dateLastEdited;
-    private String university;
+    private String universityName;
+    private String module;
+    private String uniMajor;
 }
