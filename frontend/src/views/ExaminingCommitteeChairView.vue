4a16cdd78c16a2a3be37b073797313f46b4b39d6
jakob <jakob.huss@gmail.com>
2024-03-13 20:14:43 +0100

    added new componant into view

--- frontend/src/views/ExaminingCommitteeChairView.vue
+++ frontend/src/views/ExaminingCommitteeChairView.vue
@@ -1,51 +1,19 @@
 <template>
-  <v-container fluid :key="updateKey">
-    <v-row>
-      <v-col v-for="form in formsInProgress" :key="form.timestamp">
-        <v-card>
-          <v-card-title>{{ form.universityData.universityName }}</v-card-title>
-          <v-card-subtitle>{{ form.universityData.studyProgram }}</v-card-subtitle>
-          <v-card-text>
-            <div>{{ $t("examiningCommitteeChairView.moduleCount") }}: {{ form.moduleFormsData.length }}</div>
-            <div>{{ $t("applicationForm.countryLabel") }}: {{ form.universityData.country }}</div>
-          </v-card-text>
-          <v-card-text v-if="form.comment">
-            {{ $t("examiningCommitteeChairView.studentAffairsOfficeComment") }}: {{ form.comment }}
-          </v-card-text>
-          <v-card-actions>
-            <v-btn color="green" @click="acceptForm(form.timestamp)">{{ $t("examiningCommitteeChairView.accept") }}
-            </v-btn>
-            <v-btn color="red" @click="denyForm(form.timestamp)">{{ $t("examiningCommitteeChairView.decline") }}</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-    </v-row>
+  <v-container>
+    <div>
+      <ExaminingCommitteeChairComponent/>
+    </div>
   </v-container>
 </template>
 
 <script>
+import ExaminingCommitteeChairComponent from '@/components/ExaminingCommitteeChairComponent.vue';
+
 export default {
-  computed: {
-    formsInProgress() {
-      // Get forms that are 'in progress'
-      return this.$store.getters.form.formsByStatus('in progress'); //TODO: State must be of type ENUM
-    }
-  },
-  methods: {
-    // Dispatch action to accept the form
-    acceptForm(formTimestamp) {
-      this.$store.dispatch('changeFormStatus', {
-        formId: formTimestamp,
-        newStatus: 'accepted'
-      });
-    },
-    denyForm(formTimestamp) {
-      // Dispatch action to decline the form
-      this.$store.dispatch('changeFormStatus', {
-        formId: formTimestamp,
-        newStatus: 'denied'
-      });
-    },
+  name: 'ExaminingCommitteeChairView',
+  components: {
+    ExaminingCommitteeChairComponent
   }
-}
+};
+
 </script>
