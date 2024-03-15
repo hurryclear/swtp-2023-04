5dcff95dab19bb9308daa5679521b5c377149760
jakob <jakob.huss@gmail.com>
2024-03-15 21:11:53 +0100

    renamed the ExaminingCommitteeChairView to CommitteeView

---
+++ frontend/src/views/CommitteeView.vue
@@ -0,0 +1,113 @@
+<template>
+  <v-container fluid class="wrapper">
+    <v-row>
+      <LogoutButton style="margin-left: 80%"/>
+    </v-row>
+    <v-row>
+      <v-col class="col-left">
+        <FormDisplayCommittee
+            class="form-display"
+            v-if="FDisDisplayed"
+            @open-edit-menu="openEditMenu"
+        />
+        <ComparisonMenuCommettee
+            class="comparison"
+            v-if="CMisDisplayed"
+            @close-comparison="closeComparisonMenu"
+            @open-view-application="openViewApplication"
+        />
+        <ViewApplication
+            class="view-application"
+            v-if="VAisDisplayed"
+            :form="VAformContent"
+            @close-view-application="closeViewApplication"
+        />
+      </v-col>
+      <v-col>
+        <EditMenuCommittee
+            class="edit-menu"
+            v-if="EMisDisplayed"
+            :form="EMformContent"
+            @close-edit-menu="closeEditMenu"
+            @open-comparison="openComparison"
+            @close-edit-menu-by-saving="closeEditMenuBySaving"
+        />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import EditMenuCommittee from "@/components/CommitteeViewComponants/EditMenuCommittee.vue";
+import FormDisplayCommittee from "@/components/CommitteeViewComponants/FormDisplayCommittee.vue";
+import LogoutButton from "@/components/LogoutButton.vue";
+import ComparisonMenuCommettee from "@/components/CommitteeViewComponants/ComparisonMenuCommittee.vue";
+import ViewApplication from "@/components/ViewApplication.vue";
+import axios from "@/plugins/axios";
+export default {
+  components: {ComparisonMenuCommettee, EditMenuCommittee, FormDisplayCommittee, LogoutButton, ViewApplication},
+  data() {
+    return {
+      VAisDisplayed: false,
+      EMisDisplayed: false,
+      FDisDisplayed: true,
+      CMisDisplayed: false,
+      EMformContent: {},
+      VAformContent: {}
+    }
+  },
+  methods: {
+    openEditMenu(form) {
+      this.FDisDisplayed = false;
+      this.EMisDisplayed = true;
+      axios.put(`/api/application/editingInProgress?applicationID=${form.edited.applicationData.applicationID}`)
+          .then(response => console.log(response))
+          .catch(err => console.error("Error putting status to editing in progress: ", err));
+      this.EMformContent = form;
+    },
+
+    closeEditMenu(form) {
+      this.EMisDisplayed = false;
+      this.FDisDisplayed = true;
+      axios.put(`/api/application/resetStatusInProgress?applicationID=${form.edited.applicationData.applicationID}`)
+          .then(response => console.log(response))
+          .catch(err => console.error("Error putting status to edited: ", err));
+      this.EMformContent = {};
+    },
+
+    closeEditMenuBySaving() {
+      this.EMisDisplayed = false;
+      this.EMformContent = {};
+    },
+
+    openComparison() {
+      this.FDisDisplayed = false;
+      this.CMisDisplayed = true;
+    },
+
+    closeComparisonMenu() {
+      this.CMisDisplayed = false;
+      this.FDisDisplayed = true;
+    },
+
+    openViewApplication(form) {
+      this.VAisDisplayed = true;
+      this.CMisDisplayed = false;
+      this.VAformContent = form;
+    },
+
+    closeViewApplication() {
+      this.VAisDisplayed = false;
+      this.CMisDisplayed = true;
+      this.VAformContent = {};
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+.col-left {
+  margin-left: 5%;
+}
+</style>
