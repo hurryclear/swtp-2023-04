0418dae5f70a2987b81a302ffdc1707159e816be
jakob <jakob.huss@gmail.com>
2024-03-15 21:09:15 +0100

    removed the getPdfSummary call, because it is now made inside the ReviewComponant directly

--- frontend/src/services/FormService.js
+++ frontend/src/services/FormService.js
@@ -2,18 +2,18 @@
 import axios from '@/plugins/axios';
 
 export default {
-  async fetchApplicationSummary(applicationId) {
+  async fetchApplicationSummary(applicationID) {
     try {
-      const response = await axios.get(`/api/student/reviewApplication`, {params: {applicationID: applicationId}});
+      const response = await axios.get(`/api/student/reviewApplication`, {params: {applicationID}});
       return response.data;
     } catch (error) {
       console.error('Error fetching application summary:', error);
       throw error;
     }
   },
-  async fetchPdfSummary(applicationId) {
+  async fetchPdfSummary(applicationID) {
     try {
-      const response = await axios.get(`/api/student/getPdfSummary`, {params: {applicationId}}, { responseType: 'arraybuffer' });
+      const response = await axios.get(`/api/student/getPdfSummary`, {params: {applicationID}}, { responseType: 'blob' });
       console.log(response.data);
       return response.data;
       
