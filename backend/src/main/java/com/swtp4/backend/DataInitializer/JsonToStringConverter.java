9b1945a9283e397addaf557ccf5b425c434e6d0d
Simon Knoll <116833239+simon-JK@users.noreply.github.com>
2024-03-17 20:53:42 +0100

    Added Approved Application to Startup and added status tests

---
+++ backend/src/main/java/com/swtp4/backend/DataInitializer/JsonToStringConverter.java
@@ -0,0 +1,23 @@
+package com.swtp4.backend.DataInitializer;
+
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class JsonToStringConverter {
+
+    private JsonToStringConverter() {}
+
+    public static String convertJsonToString(String filePath) throws Exception {
+        URL resource = JsonToStringConverter.class.getClassLoader().getResource(filePath);
+        if (resource == null) {
+            System.out.println("Resource not found: " + filePath);
+            System.out.println("Current working directory: " + System.getProperty("user.dir"));
+            throw new IllegalArgumentException("File not found: " + filePath);
+        }
+        Path path = Paths.get(resource.toURI());
+        byte[] bytes = Files.readAllBytes(path);
+        return new String(bytes);
+    }
+}
