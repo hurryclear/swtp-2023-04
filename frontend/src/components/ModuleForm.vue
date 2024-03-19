afbec8eceff6fac9a915ac049225d1f2ce396fbf
Robert <rs75lovo@studserv.uni-leipzig.de>
2024-03-19 16:14:34 +0100

    delete selectedUniversity on submit

--- frontend/src/components/ModuleForm.vue
+++ frontend/src/components/ModuleForm.vue
@@ -273,7 +273,6 @@ export default {
               this.moduleMapping.previousModules[index].university.name = newValue.name
               this.moduleMapping.previousModules[index].university.country = newValue.country
               this.moduleMapping.previousModules[index].university.website = newValue.web_pages.toString()
-
             }
           }
         })
